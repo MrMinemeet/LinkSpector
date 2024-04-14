e84f1e8e1fb4e1febdffcfa25c3abf296d3234e4
MrMinemeet <alex@wtf-my-code.works>
2024-04-14 08:55:09 +0200

    Add stopwatch to measure time
    Just curious how long stuff takes

--- LinkSpector/Program.cs
+++ LinkSpector/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LinkSpector;
+﻿using System.Diagnostics;
+
+namespace LinkSpector;
 
 static class Program
 {
@@ -10,8 +12,11 @@ static class Program
 		// Create a new instance of the LinkSpector class
 		LinkSpector linkSpector = new(new Uri("https://wtf-my-code.works/"));
 
+		Stopwatch stopwatch = Stopwatch.StartNew();
 		// Run the LinkSpector
 		linkSpector.Run();
+		stopwatch.Stop();
+		Console.WriteLine($"LinkSpector completed in {stopwatch.ElapsedMilliseconds}ms");
 
 		List<Result> results = linkSpector.Results;
 	}
